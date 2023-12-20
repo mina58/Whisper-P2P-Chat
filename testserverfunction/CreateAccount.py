7dd159eac7fec890b8bc773a2e43917c7d6ca945
Donia Ahmed <doniaelsharony@gmail.com>
2023-12-20 10:51:35 +0200

    show if account is existed or not

--- testserverfunction/CreateAccount.py
+++ testserverfunction/CreateAccount.py
@@ -1,25 +1,59 @@
 import socket
+import unittest
+from server.Network.ServerMainThread import ServerMainThread
 
-def send_create_account_request(host, port, username, password):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class CreateAcc(unittest.TestCase):
+    def setUp(self):
+        self.server_ip_address = socket.gethostbyname(socket.gethostname())
+        self.server_port = 12121
+        self.server_address = (self.server_ip_address, self.server_port)
+        self.server = ServerMainThread(
+            self.server_ip_address, self.server_port)
+        self.server.start()
 
-    server_address = (host, port)
-    client_socket.connect(server_address)
+    def tearDown(self):
+         self.server.stop()
 
-    try:
-        request = f"CREATE_ACC {username} {password}"
-        client_socket.sendall(request.encode())
+    def send_create_account_request():
+        
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        response = client_socket.recv(1024)
-        print(f"Server Response: {response.decode()}")
+        server_address = ( 12121)
+        client_socket.connect(server_address)
 
-    finally:
-        client_socket.close()
+        try:
+            request = f"CREATE_ACC mina123 AnaMina123"
+            client_socket.sendall(request.encode())
+
+            response = client_socket.recv(1024)
+            print(f"Server Response: {response.decode()}")
+
+        finally:
+            client_socket.close()
+
+    def test_account_creation(self): 
+        server_host ='localhost'
+        server_port = 12121 
+        new_username = 'mina123'
+        new_password = 'AnaMina123'
+
+            # self.send_create_account_request(server_host, server_port, new_username, new_password)
+            
+            # account_exists = self.server.account_dao.get_account(new_username)
+            # self.assertTrue(account_exists)
+
+        request_successful = self.send_create_account_request(server_host, server_port, new_username, new_password)
+        self.assertTrue(request_successful)
+
+        account_exists = self.server.account_dao.get_account(new_username)  
+        self.assertTrue(account_exists) 
+
+        if not request_successful:
+             non_existent_account = self.server.account_dao.get_account("non_existent_username")
+             self.assertIsNone(non_existent_account)
+       
 
 if __name__ == "__main__":
-    server_host = "localhost"
-    server_port = 12345
-    new_username = "mina123"
-    new_password = "AnaMina123"
-
-    send_create_account_request(server_host, server_port, new_username, new_password)
+    unittest.main()
+        
+        
